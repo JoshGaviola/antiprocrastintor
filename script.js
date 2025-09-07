fc57683ccca5c51c468e33235654a76e5a456792
JoshGaviola <martin100906@gmail.com>
2025-09-07 10:50:18 -0700

    test3

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
         const taskInput = document.getElementById('task-input');
         const taskList = document.getElementById('task-list');
 
-        
+        console.log("Am I working?");
         
         slider.addEventListener('input', function() {
             thresholdValue.textContent = this.value;
